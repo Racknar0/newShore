2fd80b8cc63af3f518dcb76526384dd3e21ea4ed
Camilo Torres <racknarow1@gmail.com>
2022-07-07 00:15:44 -0500

    add menu

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,74 @@
-import './Main.css'
+import './Main.css';
 
 const Main = () => {
-  return (
-    <main className='main'>
-        <div className='main__container'>
-            <p className='main__title'>Offers</p>
-
-            <div className='card__container'>
-                    <div className='card'>
-                    
-                    </div>
-                    <div className='card'>
-        
-                    </div>
-                    <div className='card'>
-        
-                    </div>
+    return (
+        <main className="main">
+            <div className="main__menu--mobile">
+                <p className="main__menu--mobile__title">Menu</p>
+                <div className="main__menu--mobile__SContainer">
+                    <i class="bi bi-caret-right-fill"></i>
+                    <p className="main__menu--mobile__text">Check-in</p>
+                </div>
+                <div className="main__menu--mobile__SContainer">
+                    <i class="bi bi-caret-right-fill"></i>
+                    <p className="main__menu--mobile__text">
+                        Manage My Booking
+                    </p>
+                </div>
+                <div className="main__menu--mobile__SContainer">
+                    <i class="bi bi-caret-right-fill"></i>
+                    <p className="main__menu--mobile__text">Search</p>
+                </div>
+                <div className="main__menu--mobile__SContainer">
+                    <i class="bi bi-caret-right-fill"></i>
+                    <p className="main__menu--mobile__text">Destinations</p>
+                </div>
+                <div className="main__menu--mobile__SContainer">
+                    <i class="bi bi-caret-right-fill"></i>
+                    <p className="main__menu--mobile__text">Members</p>
+                </div>
             </div>
-            <div className='main__menu'>
-                asd
+
+            <div className="main__container">
+                <p className="main__title">Offers</p>
+
+                <div className="card__container">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                </div>
+                <div className="main__menu">
+                    <p className="main__menu__title">Menu</p>
+                    <div className="main__menu--mobile__SContainer">
+                        <i class="bi bi-caret-right-fill"></i>
+                        <p className="main__menu--mobile__text">Check-in</p>
+                    </div>
+                    <div className="main__menu--mobile__SContainer">
+                        <i class="bi bi-caret-right-fill"></i>
+                        <p className="main__menu--mobile__text">
+                            Manage My Booking
+                        </p>
+                    </div>
+                    <div className="main__menu--mobile__SContainer">
+                        <i class="bi bi-caret-right-fill"></i>
+                        <p className="main__menu--mobile__text">Search</p>
+                    </div>
+                    <div className="main__menu--mobile__SContainer">
+                        <i class="bi bi-caret-right-fill"></i>
+                        <p className="main__menu--mobile__text">Destinations</p>
+                    </div>
+                    <div className="main__menu--mobile__SContainer">
+                        <i class="bi bi-caret-right-fill"></i>
+                        <p className="main__menu--mobile__text">Members</p>
+                    </div>
+                </div>
+                <div className="destination">
+                    <p className="destination__text">Silent Hill</p>
+                    <p className="destination__label">Chosen Destination</p>
+                </div>
             </div>
+        </main>
+    );
+};
 
-        </div>
-    </main>
-  )
-}
-
-export default Main
+export default Main;
